9ba7bb062751c5185bfff1b7dac92b76e56d1204
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-04-03 19:19:04 +0200

    Remove jsdom dependency

--- dict.js
+++ dict.js
@@ -1,32 +1,29 @@
-const https = require("https");
-const { JSDOM } = require("jsdom");
-
-let query = encodeURI(process.argv.slice(2).join(" "));
-https.get("https://www.dict.cc/?s=" + query, res => {
-  let buf = [];
-  res.on("data", data => buf.push(data));
-  res.on("end", () => {
-    let body = Buffer.concat(buf).toString("utf-8");
-    let dom = new JSDOM(body);
-    let document = dom.window.document;
-    let text = td =>
-      Array.from(td.querySelectorAll("a"))
-        .map(a => a.textContent)
-        .join(" ");
-    let trans = Array(20)
-      .fill(0)
-      .map((_, i) => ++i)
-      .map(i => document.getElementById("tr" + i))
-      .filter(tr => tr != null)
-      .map(tr => {
-        let tds = tr.querySelectorAll("td");
-        let eng = text(tds.item(1));
-        let ger = text(tds.item(2));
-        return [eng, ger];
-      });
-    let len = Math.max(...trans.map(i => i[0].length));
-    trans.forEach(([eng, ger]) => {
-      console.log(eng.padEnd(len) + " | " + ger);
-    });
-  });
-});
+#!/usr/bin/env node
+let https = require('https')
+let query = encodeURI(process.argv.slice(2).join(' '))
+https.get('https://www.dict.cc/?s=' + query, res => {
+  let buf = []
+  res.on('data', data => buf.push(data))
+  res.on('end', () => {
+    let trans = Buffer.concat(buf).toString('utf-8')
+      .match(/<tr id='tr\d+'>(.*?)<\/tr>/g)
+      .map(tr => tr.match(/<td.*?>(.*?)<\/td>/g))
+      .map(tds => [tds[1], tds[2]])
+      .map(tds => tds.map(td => td
+        .match(/<a.*?>.*?<\/a>/g)
+        .map(a =>
+          /<a.*?>(.*?)<\/a>/g.exec(a
+            .replace(/<b.*?>(.*?)<\/b>/g, '$1')
+            .replace(/<abbr.*?>(.*?)<\/abbr>/g, '$1')
+            .replace(/<kbd.*?>(.*?)<\/kbd>/g, '$1')
+            .replace('&lt;', '<')
+            .replace('&gt;', '>')
+          )[1]
+        )
+        .join(' ')
+      ))
+    if (trans.length > 15) trans.splice(15)
+    let len = Math.max(...trans.map(t => t[0].length))
+    trans.forEach(t => console.log(t[0].padEnd(len) + ' | ' + t[1]))
+  })
+})
